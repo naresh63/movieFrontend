7172d86ed1a1c79e8559d7f488cc3a37df5e25f6
naresh63 <nvalvi63@gmail.com>
2022-01-12 13:31:04 +0530

    initial commit

--- src/components/Adminpanel.js
+++ src/components/Adminpanel.js
@@ -5,8 +5,12 @@ const Adminpanel=()=>{
    let[name,setName]=useState('');
    let[genre,setGenre]=useState('');
    //
+   let[upname,setUpname]=useState('');
+   let[upgenre,setUpGenre]=useState('');
+
+//movies---------------------------------------------------------------------
     useEffect(()=>{
-        fetch("http://localhost:8000/movies") // backed name crud api
+        fetch("http://localhost:8000/movies") // backend name crud api
         .then((response)=>
             response.json() )
         .then((movies)=>{
@@ -31,30 +35,63 @@ const Adminpanel=()=>{
     }
      // add movie functionality --------------------------------------------------------------------------
         // collect input data
-    
-     const mymovi ={
-         name:name,
-         genre:genre
-     }
-    console.log(mymovi)
       // send collected data to db by fetch using POST
       function addMovie(){
-        console.log(mymovi)
-        let tempmovi=[]
           fetch(`http://localhost:8000/createMovie`,{
               method:'POST',
               headers:{
                 "Content-Type":"application/json"
                 },
-              body:JSON.stringify(mymovi)
+              body:JSON.stringify({
+                name:name,
+                genre:genre
+              })
           })
           .then(response=> response.json())
           .then((data)=>{
-              tempmovi=[...amovie,data.movie]
-              setAMovie(tempmovi); 
+              if(data !==null){
+                let  tempmovi=[...amovie,data.movie];
+                setAMovie(tempmovi); 
+              }
           })
           .catch(err=> console.log(err))
       }
+     // update functionality---------------------------------------------------------------------------------------
+     let movid=""
+     let upmovi={}
+      function updateMovie(id){
+          movid=id
+          console.log(movid)
+         let selectedMovi=amovie.filter(m=>{
+              return m._id===id
+          })
+        let[{name,genre}]=selectedMovi; // this from obj 
+         setUpname(name)
+         setUpGenre(genre)
+        //
+      }
+      // update button functionality
+      function update1movie(){
+        upmovi={
+            name:upname,
+            genre:upgenre
+        }
+          console.log(upmovi)
+        fetch("http://localhost:8000/updatemovies/"+movid,{
+            method:"PUT",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(upmovi)
+        })
+        .then((response)=> response.json()) 
+        .then((data)=>{
+              console.log(data)
+              setAMovie(amovie)
+        })
+        .catch(err=>console.log(err))     
+      }
+     //---------------------------------------------------------------------------------------------------------
     return(
         <div>
             <h1>Admin panel </h1>
@@ -66,7 +103,7 @@ const Adminpanel=()=>{
                     <input type="text" value={genre}  onChange={(event)=>{
                         setGenre(event.target.value)
                     }}/>
-                   <button onClick={ addMovie}>Add movie</button>
+                   <button onClick={()=>{addMovie()}}>Add movie</button>
                 </div> 
               {/* table for admin panel */}
             <table>
@@ -77,19 +114,28 @@ const Adminpanel=()=>{
                                 <tr key={index}>
                                    <td>{mov.name}  </td>
                                    <td>{mov.genre} </td>
-                                   <td> <button>Update</button> 
-                                      <button onClick={()=>{
-                                        deleteMovie(mov._id)
-                                        }}>Delete</button> 
+                                   <td> <button onClick={()=>{updateMovie(mov._id)}}>Update</button> 
+                                      <button onClick={()=>{deleteMovie(mov._id)}}>Delete</button>
                                     </td>
                              </tr>
                             )
                         })
                     }
-                 
                 </tbody>
-               
-            </table>
+            </table> <br/>
+            {/* update div --------------------------------------------------------------------------------*/}
+            <div className='update-model'>
+            <input type="text" value={upname}  onChange={(event)=>{
+                           setUpname(event.target.value)
+                   }}/>
+                    <input type="text" value={upgenre}  onChange={(event)=>{
+                        setUpGenre(event.target.value)
+                    }}/>
+                    <button onClick={ ()=>{
+                        update1movie()
+                    }
+                        }>update movie now </button>
+            </div>
         </div>
     )
 }
